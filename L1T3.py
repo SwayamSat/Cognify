9d3eceb77f094d98fec524bda4bbffcb986963e5
SwayamSatpathy619 <164320244+SwayamSatpathy619@users.noreply.github.com>
2024-03-21 22:38:48 +0530

    Add files via upload

---
+++ L1T3.py
@@ -0,0 +1,13 @@
+#Level 1 Task 3
+
+a=input("Enter a Email Id =")
+if "@gmail.com" in a:
+    print("Given email id is Valid")
+elif "@giet.edu" in a:
+    print("Given email id is Valid")
+elif "@yahoo.com" in a:
+    print("Given email id is Valid")
+elif "@outlook.com" in a:
+    print("Given email id is Valid")
+else:
+    print("Invalid email id")
---
+++ L2T1.py
@@ -0,0 +1,18 @@
+#Level 2 Task 2
+
+import random
+number = random.randint(1, 100)
+nguesses = 0
+print("Guess the number between 1 to 100")
+while True:
+    guess = int(input("Guess the number : "))
+    if guess > number:
+        print("Too high!")
+    elif guess < number:
+        print("Too low!")
+    else:
+        print("Congratulations")
+        print("You guessed it in ",nguesses," attempts")
+        break
+    nguesses += 1
+
